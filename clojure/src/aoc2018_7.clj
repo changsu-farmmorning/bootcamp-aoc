b459b566141ada0815d6f4dc30260cf7c37c9405
changsu <changsu.lee@greenlabs.co.kr>
2022-10-12 15:19:03 +0900

    solve 2018-7

--- clojure/src/aoc2018_7.clj
+++ clojure/src/aoc2018_7.clj
@@ -6,7 +6,7 @@
 (defn find-root
   ""
   [{:keys [done] :as m}]
-  (->> (dissoc m :now-step :root :order :done :all-steps)
+  (->> (dissoc m :now-step :root :order :done :all-steps :workers :time)
        (map (fn [[step dependencies]]
               (if (and
                     (nil? (step done))
@@ -15,7 +15,7 @@
        (keep identity)
        (into (sorted-set))))
 
-(defn make-map-with-meta-data
+(defn make-map-with-meta-data-part-1
   ""
   [m]
   (let [root (find-root m)
@@ -26,6 +26,28 @@
              :order (name first-step)
              :done #{first-step})))
 
+(defn make-map-with-meta-data-part-2
+  ""
+  [m]
+  (let [root (find-root m)
+        first-step (first root)]
+    (assoc m :now-step first-step
+             :root root
+             :all-steps (into #{} (keys m))
+             :order (name first-step)
+             :done #{}
+             :time 0
+             :workers [{:step     nil
+                        :duration 0}
+                       {:step     nil
+                        :duration 0}
+                       {:step     nil
+                        :duration 0}
+                       {:step     nil
+                        :duration 0}
+                       {:step     nil
+                        :duration 0}])))
+
 (defn parse-input
   ""
   [source]
@@ -34,10 +56,9 @@
        (map rest)
        (map #(hash-map (keyword (second %)) #{(keyword (first %))}
                        (keyword (first %)) #{}))
-       (apply merge-with clojure.set/union)
-       make-map-with-meta-data))
+       (apply merge-with clojure.set/union)))
 
-(defn take_step
+(defn take-step
   ""
   [{:keys [order] :as state}]
   (let [new-root (find-root state)
@@ -51,15 +72,153 @@
                  :now-step new-step
                  :done new-done)))
 
+(defn take-step-part-2
+  ""
+  [state]
+  (let [new-root (find-root state)]
+    (assoc state :root new-root)))
+
+
+
 (defn find-order
   ""
   [init-state]
-  (->> (iterate take_step init-state)
+  (->> (iterate take-step init-state)
+       (drop-while #(not= (:done %) (:all-steps %)))
+       first))
+
+(defn required-step-duration
+  ""
+  [step]
+  (-> step
+      name
+      char-array
+      first
+      int
+      (- 4)))
+
+(defn find-finished-job
+  ""
+  [workers]
+  (->> workers
+       (filter (fn [{:keys [step duration]}]
+                 (if (nil? step)
+                   false
+                   (= duration
+                      (required-step-duration step)))))
+       (map #(:step %))
+       (into #{})))
+
+(defn release-job
+  "[{:step :B :duration 4}]
+  [{:step nil :duration 0}]"
+  [workers finished-jobs]
+  (->> workers
+       (map (fn [{:keys [step] :as worker}]
+              (if (or (nil? step)
+                      (nil? (step finished-jobs)))
+                worker
+                (assoc worker :step nil
+                              :duration 0))))
+       (into [])))
+
+(defn check-completed-workers
+  ""
+  [{:keys [workers done] :as state}]
+  (let [finished-jobs (find-finished-job workers)
+        new-workers (release-job workers finished-jobs)]
+    (if (empty? finished-jobs)
+      state
+      (->> (assoc state :workers new-workers
+                        :done (set/union done finished-jobs))
+           take-step-part-2))))
+
+(defn working-jobs
+  ""
+  [workers]
+  (->> workers
+       (map #(:step %))
+       (keep identity)
+       (into #{})))
+
+(defn empty-worker-idx
+  ""
+  [workers]
+  (->> workers
+       (map-indexed vector)
+       (filter #(->> %
+                     second
+                     :step
+                     nil?))
+       ffirst))
+
+(defn assign-job
+  ""
+  [{:keys [jobs workers] :as origin}]
+  (let [can-assign-jobs (->> workers
+                             working-jobs
+                             (set/difference jobs)
+                             (into (sorted-set)))
+        not-assign-job (first can-assign-jobs)
+        empty-worker-idx (empty-worker-idx workers)]
+    (if (nil? empty-worker-idx)
+      origin
+      {:workers (assoc workers empty-worker-idx
+                               {:step not-assign-job :duration 0})
+       :jobs    (set/difference can-assign-jobs #{not-assign-job})})))
+
+
+(defn can-working?
+  ""
+  [workers]
+  (->> workers
+       empty-worker-idx
+       some?))
+
+(defn assign-new-jobs
+  ""
+  [{:keys [workers root] :as state}]
+  (assoc state :workers (->> (iterate assign-job {:workers workers :jobs root})
+                             (drop-while #(and (can-working? (:workers %))
+                                               (not-empty (:jobs %))))
+                             first
+                             :workers)))
+
+(defn inc-time
+  ""
+  [{:keys [workers] :as state}]
+  (-> state
+      (assoc :workers (->> workers
+                           (map #(if (some? (:step %))
+                                   (update % :duration inc)
+                                   %))
+                           (into [])))
+      (update :time inc)))
+
+(defn take-one-second
+  ""
+  [state]
+  (->> state
+       check-completed-workers
+       assign-new-jobs
+       inc-time))
+
+
+(defn find-take-time
+  ""
+  [init-state]
+  (->> (iterate take-one-second init-state)
        (drop-while #(not= (:done %) (:all-steps %)))
        first))
 
 (comment
   (->> (parse-input "resources/day7_sample.txt")
+       make-map-with-meta-data-part-1
        find-order)
   (->> (parse-input "resources/day7.txt")
-       find-order)) =
+       make-map-with-meta-data-part-1
+       find-order)
+  (->> (parse-input "resources/day7.txt")
+       make-map-with-meta-data-part-2
+       assign-new-jobs
+       find-take-time))
